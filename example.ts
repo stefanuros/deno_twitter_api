5e9e4fd7f6b13f23a8e1e879391e4dfdf0e01f36
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-01-22 01:56:41 +0100

    Update example to reflect V2

--- example.ts
+++ example.ts
@@ -3,6 +3,7 @@ import { keys } from './keys.ts';
 
 // See correct format for keys in twitterApi.ts
 let twitterApi = new TwitterApi(keys);
+let twitterApi2 = new TwitterApi(keys, {apiVersion: '2'});
 
 let userTimeline = await twitterApi.get("statuses/user_timeline.json", {
   user_id: "19025957",
@@ -22,8 +23,6 @@ let alternativeGet = await twitterApi.request("GET", "statuses/user_timeline.jso
   tweet_mode: "extended", // Used to prevent truncating tweets
 });
 
-// These post requests do not work in the current state and are here
-// to show how someone would be able to use the post function
 let postResult = await twitterApi.post("statuses/update.json", {
   status: "Hello Ladies + Gentlement, a signed OAuth request!"
 });
@@ -31,3 +30,9 @@ let postResult = await twitterApi.post("statuses/update.json", {
 let alternativePost = await twitterApi.request("POST", "statuses/update.json", {
   status: "Hello Ladies + Gentlement, a signed OAuth request!"
 });
+
+const tweetInfo = (await (await twitterApi2.get('tweets/1320822556614676480', {
+  'tweet.fields': 'referenced_tweets',
+  'user.fields': 'username'
+})).json());
+console.log(data);
